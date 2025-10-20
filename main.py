8e6cf1b04d62a803c98b6b0aec5c0fcbd7ee19b1
natelallen <nathanlane.allen@gmail.com>
2025-10-20 11:43:32 -0400

    added asteroid spawning

--- main.py
+++ main.py
@@ -4,21 +4,29 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField 
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width:",SCREEN_WIDTH)
     print(f"Screen height:",SCREEN_HEIGHT)
 
-    Player.updatable = pygame.sprite.Group()    
-    Player.drawables = pygame.sprite.Group()
-    Player.containers = Player.updatable, Player.drawables
+    drawables = pygame.sprite.Group()    
+    updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawables)
+    Player.containers = (updatable, drawables)
+    AsteroidField.containers = (updatable,)
+    
 
 # initialize pygame, define screen
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    asteroid_field = AsteroidField()
     dt = 0
 
 # infinite loop to run the game window
@@ -27,13 +35,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        for drawable in Player.drawables:
-            drawable.draw(screen)
-
-        Player.updatable.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        for drawable in Player.drawables:
+        for drawable in drawables:
             drawable.draw(screen)
         pygame.display.flip()
 
